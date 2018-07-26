219fc4821761561c6ff8f709e1c8a0146feb114a
Mario <the4power@yahoo.com.ar>
2018-07-26 12:02:23 -0300

    fix include

--- test_new/main.cpp
+++ test_new/main.cpp
@@ -18,13 +18,43 @@
  */
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+//#include <bitprim/keoken/interpreter.hpp>
+
 #include "doctest.h"
 
-#ifdef BITPRIM_CURRENCY_BCH
-TEST_CASE("[test1] test1") {
+#include <bitprim/keoken/state.hpp>
+
+using namespace bitprim::keoken;
+
+TEST_CASE("[state_empty] ") {
+
+    state state_(1);
+    //interpreter interpreter_;
+
+    /*  void create_asset(message::create_asset const& msg, 
+                      payment_address const& owner,
+                      size_t block_height, libbitcoin::hash_digest const& txid);
+
+    //TODO(fernando): message::send_tokens should not be used here
+    void create_balance_entry(message::send_tokens const& msg, 
+                              payment_address const& source,
+                              payment_address const& target, 
+                              size_t block_height, libbitcoin::hash_digest const& txid);
+
+    bool asset_id_exists(domain::asset_id_t id) const;
+
+    domain::amount_t get_balance(domain::asset_id_t id, payment_address const& addr) const;
+
+
+    get_assets_by_address_list get_assets_by_address(libbitcoin::wallet::payment_address const& addr) const;
+    get_assets_list get_assets() const;
+    get_all_asset_addresses_list get_all_asset_addresses() const;*/
+
+    //auto const& ret = state_.get_assets();
 
     CHECK(true);
+    //CHECK( ! state_.asset_id_exists(1));
 }
-#endif //BITPRIM_CURRENCY_BCH
 
 
